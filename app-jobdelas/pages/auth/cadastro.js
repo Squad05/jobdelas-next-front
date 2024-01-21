b567e3a9dba128991f09c01328f1b1f8b6d168d4
MeDourado <mariaeduarda.s.dourado@gmail.com>
2024-01-21 19:51:05 -0300

    Subindo inicio do projeto

---
+++ app-jobdelas/pages/auth/cadastro.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Cadastro() {
+  return (
+    <>
+      <Head>
+        <title>Jobdelas - Cadastro </title>
+      </Head>
+      <main></main>
+    </>
+  );
+}
---
+++ app-jobdelas/pages/auth/login.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Jobdelas - Login </title>
+      </Head>
+      <main></main>
+    </>
+  );
+}
