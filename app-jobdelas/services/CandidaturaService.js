b904ec445334bf5112aadc8fc98bd95a01b44072
MeDourado <mariaeduarda.s.dourado@gmail.com>
2024-01-28 09:52:15 -0300

    Aplicação candidatura finalizada

---
+++ app-jobdelas/services/CandidaturaService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+class CandidaturaService {
+  async cadastrarCandidaturaVaga(candidatura) {
+    try {
+      const response = await axios.post(
+        "https://jbcompanyapi.onrender.com/candidaturas",
+        candidatura
+      );
+
+      console.log(response.data);
+
+      return response.data;
+    } catch (error) {
+      console.error("Erro ao cadastrar nova candidatura:", error);
+      throw error;
+    }
+  }
+}
+
+export default new CandidaturaService();
